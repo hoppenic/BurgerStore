e34b43b2b368e0aae4bfe38d78c7eaf4df1a4944
hoppenic <hoppenic@gmail.com>
2018-05-02 14:29:00 -0500

    update

---
+++ MeatStore/Controllers/ProductController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MeatStore.Models;
+
+
+namespace MeatStore.Controllers
+{
+    public class ProductController : Controller
+    {
+        private List<Product> _products;
+
+        public ProductController()
+        {
+            _products = new List<Product>();
+            _products.Add(new Product
+            {
+                ID = 1,
+                Name = " Traditional Ground Beef Burger",
+                Description = "Ground Beef",
+                Image =" ",
+                Price = 5.99m,
+                Organic=false
+
+            });
+            _products.Add(new Product
+            {
+                ID = 2,
+                Name = " Traditional Ground Turkey Burger",
+                Description = "Ground Turkey",
+                Image = " ",
+                Price = 4.99m,
+                Organic = false
+
+            });
+        }
+
+
+        
+
+
+        public IActionResult Index()
+        {
+            return View(_products);
+        }
+
+        //This is the details method connecting to details view
+        public IActionResult Details(int? ID)
+        {
+            if (ID.HasValue)
+            {
+                Product p = _products.Single(x => x.ID == ID.Value);
+                return View(p);
+            }
+            return NotFound();
+        }
+
+
+
+
+    }
+}
---
+++ MeatStore/Models/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MeatStore.Models
+{
+    public class Product
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal? Price { get; set; }
+        public string Image { get; set; }
+        public bool Organic { get; set; }
+        public bool Grassfed { get; set; }
+
+
+        //can add many more properties here for my project
+
+
+
+
+    }
+}
