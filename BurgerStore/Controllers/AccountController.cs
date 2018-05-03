475e17ecc105dd9299b6709d070ecd2be4c42fa2
hoppenic <hoppenic@gmail.com>
2018-05-03 13:14:46 -0500

    update

---
+++ BurgerStore/Controllers/AccountController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BurgerStore.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        //Responds on GET /account/register
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        //Responds on POST /account/register
+        [HttpPost]
+        [ValidateAntiForgeryToken] //this prevents automated scripts from trying to login
+        public IActionResult Register(Models.RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                //TODO: Create account to log user in
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View();
+        }
+    }
+}
---
+++ BurgerStore/Models/RegisterViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BurgerStore.Models
+{
+    public class RegisterViewModel
+    {
+
+        [System.ComponentModel.DataAnnotations.Required]
+        public string Email { get; set; }
+
+        [System.ComponentModel.DataAnnotations.MinLength(8,ErrorMessage ="Password must be at least 8 characters in length.")]
+        [System.ComponentModel.DataAnnotations.Required]
+        public string Password { get; set; }
+
+
+
+    }
+}
